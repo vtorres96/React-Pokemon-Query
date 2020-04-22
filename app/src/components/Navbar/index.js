8e596848bbc9a883dce2cdd715c36fe6297a22fa
vtorres96 <victorcdc96@gmail.com>
2020-04-22 01:28:10 -0300

    added Navbar component

---
+++ app/src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            Pokemon Query
+        </div>
+    );
+}
+
+export default Navbar;
